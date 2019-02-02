ca61691ef40a194c52774f9497c35d3581fb545a
Henrick Deschamps <henrick.deschamps@gmail.com>
2019-02-02 19:26:03 +0100

    Implementing simple singleton

--- creational-patterns/singleton/main.cpp
+++ creational-patterns/singleton/main.cpp
@@ -1,1 +1,52 @@
-int main(void) { return 0; }
+#include <iostream>
+#include <memory>
+#include <vector>
+
+using std::cout;
+using std::endl;
+using std::shared_ptr;
+using std::vector;
+
+using SpA = shared_ptr<class A>;
+
+class A final {
+private:
+  A() = default;
+
+public:
+  static SpA &get_instance() {
+    static auto &&instance = SpA(new A());
+    return (instance);
+  }
+
+  ~A() = default;
+  A(const A &) = delete;
+
+  void operator=(const A &) = delete;
+
+  A(A &&) = default;
+
+  A &operator=(A &&) = default;
+
+  void call() const {
+    static auto counter = 0;
+    cout << this << " " << counter++ << endl;
+  }
+};
+
+int main() {
+  auto v_sp_a = vector<SpA>();
+
+  v_sp_a.push_back(A::get_instance());
+  v_sp_a.push_back(A::get_instance());
+
+  for (const auto &sp_a : v_sp_a) {
+    sp_a->call();
+  }
+
+  for (const auto &sp_a : v_sp_a) {
+    sp_a->call();
+  }
+
+  return 0;
+}
